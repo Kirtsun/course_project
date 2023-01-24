88d205700279c1d3f2134dfe271420e8d084661b
Kyrtsun <olegkirtsun@gmail.com>
2023-01-24 21:36:06 +0200

    cart

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('book_list/', views.BookList.as_view(), name='book_list'),
-    path('book_detail/<int:pk>', views.book_detail, name='book_detail')
-
+    path('book_detail/<int:pk>', views.book_detail, name='book_detail'),
+    path('order_create/', views.order_create, name='order_create')
 ]
--- shop/shop/views.py
+++ shop/shop/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Book, Order
+from .models import Book, Order, OrderItem
+from .forms import OrderCreateForm
 from django.views import generic
 from cart.forms import CartAddBookForm
+from cart.cart import Cart
 
 
 class BookList(generic.ListView):
@@ -15,3 +17,25 @@ def book_detail(request, pk):
     book = get_object_or_404(Book, id=pk)
     cart_book_form = CartAddBookForm()
     return render(request, 'shop/book_detail.html', {'book': book, 'cart_book_form': cart_book_form})
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.user_id = request.user
+            order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         book=item['book'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+
+            cart.clear()
+            return render(request, 'shop/order_created.html',
+                          {'order': order})
+    else:
+        form = OrderCreateForm
+    return render(request, 'shop/order_create.html', {'cart': cart, 'form': form})
